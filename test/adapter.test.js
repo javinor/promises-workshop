269e839b6ae9671ef7eeec0b04b67d6e383a2296
Jonathan Avinor <javinor@gmail.com>
2021-08-26 15:21:42 +0300

    fix bug in test summary

--- test/adapter.test.js
+++ test/adapter.test.js
@@ -4,8 +4,9 @@ const {myAdapter} = require("../src/adapter")
 promisesAplusTests(myAdapter, function (err) {
   // All done; output is in the console. Or check `err` for number of failures.
   const totalNumberOfTests = 872
+  const numberOfFailures = err ? err.failures : 0
   console.log("========== Summary ==========")
-  console.log("Number of passing tests: ", totalNumberOfTests - err.failures)
-  console.log("Number of failing tests: ", err.failures)
+  console.log("Number of passing tests: ", totalNumberOfTests - numberOfFailures)
+  console.log("Number of failing tests: ", numberOfFailures)
   console.log("=============================")
 });
