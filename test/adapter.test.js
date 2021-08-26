dfb23045244cc97e99aa33f88d3e65a08cc6feed
Jonathan Avinor <javinor@gmail.com>
2021-08-26 11:59:44 +0300

    print summary of passing/failing tests

--- test/adapter.test.js
+++ test/adapter.test.js
@@ -3,4 +3,9 @@ const {myAdapter} = require("../src/adapter")
 
 promisesAplusTests(myAdapter, function (err) {
   // All done; output is in the console. Or check `err` for number of failures.
+  const totalNumberOfTests = 872
+  console.log("========== Summary ==========")
+  console.log("Number of passing tests: ", totalNumberOfTests - err.failures)
+  console.log("Number of failing tests: ", err.failures)
+  console.log("=============================")
 });
